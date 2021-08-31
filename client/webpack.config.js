60dbcffd890f5894220f325056d5f9c09af71268
Archrona <ken@kengrauel.dev>
2021-08-31 18:14:57 -0400

    Built webpack/babel client framework

---
+++ client/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  mode: 'development', 
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
